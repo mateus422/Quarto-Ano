b8ed03d43dded99eae8a7b3f5f523c716eb7023d
Mateus4222 <mateus13.aa@hotmail.com>
2017-10-25 02:02:53 -0300

    3° questão V.0.8

--- 3° Exercicio/setcookie.php
+++ 3° Exercicio/setcookie.php
@@ -1,10 +1,5 @@
 <?php
-$pagina = null;
-if (!$pagina) {
-	$pagina=1;
-}else{
-$pagina = $_GET['pagina'];
-}
+
 /*function definirCookie1(){
 	setcookie("user", "Ricardo Rubens", time()+3600);
 if ( !$_COOKIE["qtd"] ){
@@ -27,32 +22,129 @@ echo "todos: <br/>";
 print_r($_COOKIE);
 }*/
 
-function definirCookie2($pagina){
-	setcookie("user", "Ricardo Rubens", time()+3600);
-if ( !$_COOKIE["qtd"] ){
-	setcookie("qtd", 1, time()+3600);
+
+
+function cookie1(){
+   setcookie("paginas", "1", time()+3600);
+if ( !$_COOKIE["qtd1"] ){
+	setcookie("qtd1", 0, time()+3600);
 }else{
-	setcookie("qtd", $_COOKIE["qtd"] + 1,time()+3600);
+	setcookie("qtd1", $_COOKIE["qtd1"] + 1,time()+3600);
 }
-echo "<html>";
-// Imprimir um cookie
-echo "oi, estou na pagina $pagina";
-echo "<a href='pagina.php?pagina=1'>Pagina 1</a> <br>
-	<a href='pagina.php?pagina=2'>Pagina 2</a> <br>
-	<a href='pagina.php?pagina=3'>Pagina 3</a> <br>
-	<a href='pagina.php?pagina=4'>Pagina 4</a> <br>
-	<a href='pagina.php?pagina=5'>Pagina 5</a> <br>
-	<a href='pagina.php?pagina=6'>Pagina 6</a> <br>
-	";
-echo "cookie user: ".$_COOKIE["user"];
-echo "<hr/>";
-echo "todos: <br/>";
-// Ver todos os cookies
+
 print_r($_COOKIE);
 }
 
+function cookie2(){
+    setcookie("paginas", "2", time()+3600);
+if ( !$_COOKIE["qtd2"] ){
+	setcookie("qtd2", 0, time()+3600);
+}else{
+	setcookie("qtd2", $_COOKIE["qtd2"] + 1,time()+3600);
+}
+    
+print_r($_COOKIE);
+}
 
-definirCookie2($pagina);
+function cookie3(){
+    setcookie("paginas", "3", time()+3600);
+if ( !$_COOKIE["qtd3"] ){
+	setcookie("qtd3", 0, time()+3600);
+}else{
+	setcookie("qtd3", $_COOKIE["qtd3"] + 1,time()+3600);
+}
+    
 
+print_r($_COOKIE);
+}
+
+function cookie4(){
+    setcookie("paginas", "4", time()+3600);
+if ( !$_COOKIE["qtd4"] ){
+	setcookie("qtd4", 0, time()+3600);
+}else{
+	setcookie("qtd4", $_COOKIE["qtd4"] + 1,time()+3600);
+}
+    
+
+print_r($_COOKIE);
+}
+
+function cookie5(){
+    setcookie("paginas", "5", time()+3600);
+if ( !$_COOKIE["qtd5"] ){
+	setcookie("qtd5", 0, time()+3600);
+}else{
+	setcookie("qtd5", $_COOKIE["qtd5"] + 1,time()+3600);
+}
+    
+
+print_r($_COOKIE);
+}
+
+function cookie6(){
+    setcookie("paginas", "6", time()+3600);
+if ( !$_COOKIE["qtd6"] ){
+	setcookie("qtd6", 0, time()+3600);
+}else{
+	setcookie("qtd6", $_COOKIE["qtd6"] + 1,time()+3600);
+}
+    
+
+print_r($_COOKIE);
+}
+
+function principal($pagina){
+echo "<html>";
+    
+    echo "Estou na pagina  ".$pagina."<>";
+// Imprimir um cookie
+echo "<a href='setcookie.php?pagina=1'>Pagina 1</a> <br>
+	<a href='setcookie.php?pagina=2'>Pagina 2</a> <br>
+	<a href='setcookie.php?pagina=3'>Pagina 3</a> <br>
+	<a href='setcookie.php?pagina=4'>Pagina 4</a> <br>
+	<a href='setcookie.php?pagina=5'>Pagina 5</a> <br>
+	<a href='setcookie.php?pagina=6'>Pagina 6</a> <br>
+	";
+
+echo "<hr/>";
+}
+
+
+$GLOBALS['pagina'] = $_GET['pagina'];
+
+switch($GLOBALS['pagina']){
+    case 1:
+        principal($GLOBALS['pagina']);
+        cookie1();
+        
+        break;
+    case 2:
+        principal($GLOBALS['pagina']);
+        cookie2();
+        
+        break;
+    case 3:
+        principal($GLOBALS['pagina']);
+        cookie3();
+        
+        break;
+    case 4:
+        principal($GLOBALS['pagina']);
+        cookie4();
+        
+        break;
+    case 5:
+        principal($GLOBALS['pagina']);
+        cookie5();
+       
+        break;
+    case 6:
+        principal($GLOBALS['pagina']);
+        cookie6();
+        
+        break;
+        
+    }
 
 ?>
